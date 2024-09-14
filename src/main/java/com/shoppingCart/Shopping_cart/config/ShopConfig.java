6cb6ff9ad5197460d856ae1816e8094150d923b5
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2024-09-14 23:16:59 +0700

    up shop config, product dto

--- src/main/java/com/shoppingCart/Shopping_cart/config/ShopConfig.java
+++ src/main/java/com/shoppingCart/Shopping_cart/config/ShopConfig.java
@@ -1,2 +1,15 @@
-package com.shoppingCart.Shopping_cart.config;public class ShopConfig {
+package com.shoppingCart.Shopping_cart.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShopConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
 }
