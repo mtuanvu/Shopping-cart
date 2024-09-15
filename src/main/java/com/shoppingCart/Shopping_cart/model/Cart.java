d77270b5c6d428fd46c9e263d7098cc1b9328382
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2024-09-15 15:17:22 +0700

    update cart entity, cartItemService, icartItemService

--- src/main/java/com/shoppingCart/Shopping_cart/model/Cart.java
+++ src/main/java/com/shoppingCart/Shopping_cart/model/Cart.java
@@ -24,20 +24,25 @@ public class Cart {
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<CartItem> items = new HashSet<>();
 
-    public void addItem(CartItem item){
+    public void addItem(CartItem item) {
         this.items.add(item);
         item.setCart(this);
         updateTotalAmount();
     }
 
-    private void updateTotalAmount(){
-        this.totalAmount = items.stream()
-                .map(item -> {
-                    BigDecimal  unitPrice = item.getUnitPrice();
-                    if (unitPrice == null){
-                        return BigDecimal.ZERO;
-                    }
-                    return unitPrice.multiply(BigDecimal.valueOf(item.getQuantity()));
-                }).reduce(BigDecimal.ZERO, BigDecimal::add);
+    public void removeItem(CartItem item) {
+        this.items.remove(item);
+        item.setCart(null);
+        updateTotalAmount();
+    }
+
+    private void updateTotalAmount() {
+        this.totalAmount = items.stream().map(item -> {
+            BigDecimal unitPrice = item.getUnitPrice();
+            if (unitPrice == null) {
+                return  BigDecimal.ZERO;
+            }
+            return unitPrice.multiply(BigDecimal.valueOf(item.getQuantity()));
+        }).reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
--- src/main/java/com/shoppingCart/Shopping_cart/service/cart/ICartItemService.java
+++ src/main/java/com/shoppingCart/Shopping_cart/service/cart/ICartItemService.java
@@ -1,4 +1,12 @@
 package com.shoppingCart.Shopping_cart.service.cart;
 
+
+import com.shoppingCart.Shopping_cart.model.CartItem;
+
 public interface ICartItemService {
+    void addItemToCart(Long cartId, Long productId,int quantity);
+    void removeItemFromCart(Long cartId, Long productId);
+    void updateItemQuantity(Long cartId, Long productId, int quantity);
+
+    CartItem getCartItem(Long cartId, Long productId);
 }
