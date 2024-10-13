68e2053bad2ff73ea8d8c6318da9ea7f566dd29f
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2024-10-13 09:48:56 +0700

    add method register

--- src/main/java/com/shoppingCart/Shopping_cart/controller/AuthController.java
+++ src/main/java/com/shoppingCart/Shopping_cart/controller/AuthController.java
@@ -1,10 +1,15 @@
 package com.shoppingCart.Shopping_cart.controller;
 
+import com.shoppingCart.Shopping_cart.dto.UserDto;
+import com.shoppingCart.Shopping_cart.exceptions.AlreadyExistsException;
+import com.shoppingCart.Shopping_cart.model.User;
+import com.shoppingCart.Shopping_cart.request.CreateUserRequest;
 import com.shoppingCart.Shopping_cart.request.LoginRequest;
 import com.shoppingCart.Shopping_cart.response.ApiResponse;
 import com.shoppingCart.Shopping_cart.response.JwtResponse;
 import com.shoppingCart.Shopping_cart.secirity.jwt.JwtUtils;
 import com.shoppingCart.Shopping_cart.secirity.user.ShopUserDetails;
+import com.shoppingCart.Shopping_cart.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,12 +24,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.HttpStatus.CONFLICT;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${api.prefix}/auth")
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
+    private final UserService userService;
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse> login(@Valid @RequestBody LoginRequest request){
@@ -41,6 +49,18 @@ public class AuthController {
             return ResponseEntity.ok(new ApiResponse("Login Successful", jwtResponse));
         } catch (AuthenticationException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse> resgister(@RequestBody CreateUserRequest request){
+        try {
+            User user = userService.createUser(request);
+            UserDto userDto = userService.convertUserToUserDto(user);
+            return ResponseEntity.ok(new ApiResponse("Register Successfully!", userDto));
+        }catch (AlreadyExistsException e){
+            return ResponseEntity.status(CONFLICT)
                     .body(new ApiResponse(e.getMessage(), null));
         }
     }
